ab60b6f8db7d6b399f470c6869f6991dadf7d17a
Victor Hugo Alves <vgoncalves360@hotmail.com>
2026-03-01 21:20:45 -0300

    fix: Use VITE_REVERB_HOST environment variable for WebSocket connection

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -17,7 +17,7 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: window.location.hostname,
+    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
